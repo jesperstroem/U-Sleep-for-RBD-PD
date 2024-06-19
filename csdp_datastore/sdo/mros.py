09695b6cca21ce8d3cdf8cc6d526bfd3e6e65e72
jesperstroem <js@ece.au.dk>
2024-06-19 16:21:34 +0200

    Added extra m1-m2 config

--- csdp_datastore/sdo/mros.py
+++ csdp_datastore/sdo/mros.py
@@ -11,6 +11,8 @@ class MROS(SleepdataOrg):
             "C4": Mapping(TTRef.C4, TTRef.Fpz),
             "A1": Mapping(TTRef.LPA, TTRef.Fpz),
             "A2": Mapping(TTRef.RPA, TTRef.Fpz),
+            "M1": Mapping(TTRef.LPA, TTRef.Fpz),
+            "M2": Mapping(TTRef.RPA, TTRef.Fpz),
             "ROC": Mapping(TTRef.ER, TTRef.Fpz),
             "LOC": Mapping(TTRef.EL, TTRef.Fpz)
         }
