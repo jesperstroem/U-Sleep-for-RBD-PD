e15a290ae813aa8fc2bafd1bf787f01e60084b61
jesperstroem <js@ece.au.dk>
2025-02-20 17:02:59 +0100

    docstring to bids predictor

--- csdp_training/experiments/bids_predictor.py
+++ csdp_training/experiments/bids_predictor.py
@@ -20,7 +20,7 @@ class BIDS_USleep_Predictor():
             sessions (list(str), optional): List of sessions, e.g. ["ses-001"]. Defaults to None, which means it will sleepstage every session.
         """
         assert (data_extension==".vhdr") or (data_extension==".set") or (data_extension==".edf")
-        
+
         self.data_dir = data_dir
         self.data_extension = data_extension
         self.data_task = data_task
@@ -29,15 +29,31 @@ class BIDS_USleep_Predictor():
 
         self.filepaths = mb.find_matching_paths(data_dir,extensions=data_extension,tasks=data_task,subjects=subjects, sessions=sessions)
 
-    def get_available_channels(self, overlapping=False):
+    def get_available_channels(self):
+        """Helper function to obtain channel information from the BIDS dataset
+
+        Returns:
+            set: Names of the available channels
+        """
         ch_names = sleep_dataset_from_paths.get_available_channels(self.filepaths)
 
-        if overlapping==True:
-            return set.intersection(*[set(x) for x in ch_names])
-        else:
-            return ch_names
+        #if overlapping==True:
+        return set.intersection(*[set(x) for x in ch_names])
+        ##else:
+         #   return ch_names
         
     def predict_all(self, lighting_checkpoint, dataset, eeg_indexes=None, eog_indexes=None):
+        """Function to predict on a dataset object, given a U-Sleep checkpoint
+
+        Args:
+            lighting_checkpoint (str): Path to a U-Sleep Lightning checkpoint
+            dataset (Torch Dataset): A Torch dataset, obtained with the function "build_dataset"
+            eeg_indexes (list(int), optional): Indexes for which channels in the dataset are eeg. Can't be None if you have specified a two-channel model. Defaults to None.
+            eog_indexes (list(int), optional): Indexes for which channels in the dataset are eog. Can't be None if you have specified a two-channel model. Defaults to None.
+
+        Returns:
+            list(torch.Tensor): A list of sleep-stage predictions, one for each entry in the BIDS dataset
+        """
         fac = USleep_Factory(lr=0.0001, batch_size=64)
         usleep: USleep_Lightning = fac.create_pretrained_net(lighting_checkpoint)
         num_channels = usleep.num_channels
@@ -80,6 +96,14 @@ class BIDS_USleep_Predictor():
         return all_preds
 
     def build_dataset(self, channel_names: list):
+        """Builds a Dataset object from a list of channel names
+
+        Args:
+            channel_names (list(str)): List with channel names. Avaiable channel names can be obtained from the function "get_available_channels"
+
+        Returns:
+            Torch Dataset: A Torch dataset containing the given channel names
+        """
         data_set = sleep_dataset_from_paths(self.filepaths,
                                             scoring_paths=[],
                                             ch_names=channel_names,
